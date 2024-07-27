b399fc1f1160b6e9b8b07a2ef04e74e4244fee9b
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2024-07-26 22:19:17 -0500

    feat:products-register-firebase

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,6 +1,39 @@
+import { addDoc, collection } from 'firebase/firestore';
 import {defineStore} from 'pinia'
-
+import { computed } from 'vue';
+import { useFirestore,useCollection, useFirebaseStorage } from 'vuefire'
 
 export const useProductsStore = defineStore('products',() => {
+   const db = useFirestore();
+   const storage = useFirebaseStorage();
+
+
+   const categories = [
+      {id:1,name:'Polos'},
+      {id:2,name:'Camisas'},
+      {id:3,name:'Zapatillas'},
+      {id:4,name:'Relojes'},
+      {id:5,name:'Carteras'},
+   ]
+
+   async function createProduct(product){
+      await addDoc(collection(db,'products'),product)
+   }
+
+   const categoryOptions = computed(()=>{
+      const options = [
+         {label:'Seleccione',value:'',attrs:{disabled:true}},
+         ...categories.map(category => ({
+            label:category.name,
+            value:category.id}
+         ))
+      ]
+      return options
+   })
+
+   return {
+      createProduct,
+      categoryOptions
+   }
 
 })
--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -1,5 +1,32 @@
 <script setup>
 import Link from '@/components/Link.vue'
+import { useRouter } from 'vue-router';
+import {useProductsStore} from '@/stores/products'
+import { reactive } from 'vue';
+
+const products = useProductsStore();
+const router = useRouter();
+const formData = reactive({
+   name:'',
+   price:'',
+   stock:'',
+   category:'',
+   image: '',
+})
+
+
+const submitHandler = async (data) =>{
+const {image,...values} = data;
+   try {
+      await products.createProduct({
+         ...values,
+      })
+      router.push({name: 'productos'})
+   } catch (error) {
+      console.log(error)
+   }
+}
+
 </script>
 
 <template>
@@ -12,36 +39,72 @@ import Link from '@/components/Link.vue'
       <h1 class="text-4xl font-bold my-8">Nuevo Producto</h1>
       <div class="flex justify-center bg-white shadow max-w-4xl mx-auto">
          <div class="mt-2 p-10 w-full 2xl:w-4/4">
-         <FormKit type="form" submit-label="Agregar Producto">
-          <FormKit
-            type="text"
-            label="Nombre"
-            name="name"
-            validation = "required"
-            placeholder="Nombre del producto"
-          />
+         <FormKit 
+            type="form" 
+            submit-label="Agregar Producto"
+            :value="formData" 
+            @submit="submitHandler"
+         >
+          
+            <FormKit
+               type="text"
+               label="Nombre"
+               name="name"
+               validation = "required"
+               placeholder="Nombre del producto"
+               v-model.trim="formData.name"
+               v-bind:validation-messages="{
+                  required: 'El nombre del producto es obligatorio'
+               }"
+            />
 
-          <FormKit type="file" label="Imagen Producto" name="image" />
+         <FormKit 
+            type="file" 
+            label="Imagen Producto" 
+            name="image" 
+            validation = "required"
+            v-bind:validation-messages="{
+               required: 'La imagen del producto es obligatorio'
+            }"
+            v-model.trim="formData.image"
+         />
 
           <FormKit
             type="select"
             label="Categoria"
             name="category"
-            :options="['Electronica', 'Hogar', 'Oficina']"
+            validation = "required"
+            :options="products.categoryOptions"
+            v-bind:validation-messages="{
+               required: 'La categoria del producto es obligatorio'
+            }"
+            v-model.number="formData.category"
           />
 
           <FormKit
             type="number"
             label="Precio"
             name="price"
+            validation = "required"
             placeholder="Precio del producto"
+            v-bind:validation-messages="{
+               required: 'El precio del producto es obligatorio'
+            }"
+            v-model.number="formData.price"
+            min="1"
           />
 
           <FormKit
             type="number"
             label="Stock"
             name="stock"
+            validation = "required"
             placeholder="Cantidad de disponible"
+            v-bind:validation-messages="{
+               required: 'Debe ingresar Stock del producto'
+            }"
+            v-model.number="formData.stock"
+            min="1"
           />
         </FormKit>
       </div>
